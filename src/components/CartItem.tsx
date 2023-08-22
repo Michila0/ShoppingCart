d3bff79a7b7301d9bb79d09037c835b162a35653
Michila <michila.bimsara12@gmail.com>
2023-08-22 16:39:16 +0530

    feat: display the item name on the cart

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -13,7 +13,7 @@ export function CartItem({id, quantity}: CartItemProps) {
     if(item == null) return null
 
     return (
-        <Stack direction="horizontal" gap={2}>
+        <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
             <img src={item.imgUrl} 
                 style={{
                     width: "125px",
@@ -21,6 +21,17 @@ export function CartItem({id, quantity}: CartItemProps) {
                     objectFit: "cover"
                 }} 
             />
+            <div className="me-auto">
+                <div>
+                    {item.name}
+                    {quantity > 1 && (
+                        <span className="text-muted" style={{fontSize: "65rem"}}>
+                            x{quantity}
+                        </span>
+                    )}
+                </div>
+                <div className=""></div>
+            </div>
         </Stack>
     )
 }
